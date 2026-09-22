18905adc0fcedc4727a4379d12b4a942135cf28b
Ahmed Waleed Ameen <126421445+amf5@users.noreply.github.com>
2026-09-22 21:03:25 +0300

    Refactor getTimeByTimezone to use query params

--- calculator-hub-frontend/src/services/time.service.js
+++ calculator-hub-frontend/src/services/time.service.js
@@ -2,7 +2,12 @@ import api from "./api";
 
 export const getTimeByTimezone = async (timezone) => {
     const response = await api.get(
-        `/api/v1/time/${timezone}`
+        "/api/v1/time",
+        {
+            params: {
+                timezone,
+            },
+        }
     );
 
     return response.data;
